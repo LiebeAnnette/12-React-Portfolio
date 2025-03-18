bbbde2758f833f81fe18deb814902907bddc5118
LiebeAnnette <liebeannette@gmail.com>
2025-03-18 18:47:23 -0500

    added icon links in footer to GitHub, LinkedIn and Stack Overflow

--- ReactPortfolioProject/src/components/Footer.jsx
+++ ReactPortfolioProject/src/components/Footer.jsx
@@ -1,7 +1,32 @@
+import { FaGithub, FaLinkedin, FaStackOverflow } from "react-icons/fa";
+
 export default function Footer() {
-	return (
-		<footer>
-			<p>© 2021 React Portfolio Project</p>
-		</footer>
-	);
+  return (
+    <footer>
+      <p>© 2025 React Portfolio Project</p>
+      <p>
+        <a
+          href="https://github.com/liebeannette"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={24} />
+        </a>{" "}
+        <a
+          href="https://linkedin.com/in/liebe-stevenson-116ba97a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={24} />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/28229648/liebe"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaStackOverflow size={24} />
+        </a>
+      </p>
+    </footer>
+  );
 }
