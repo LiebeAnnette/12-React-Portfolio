5db0b570b1a288c9e230fe60440f061edfd9ddc8
LiebeAnnette <liebeannette@gmail.com>
2025-03-18 20:34:40 -0500

    added photo and placeholder for short bio to About Me page

--- ReactPortfolioProject/src/components/AboutMe.jsx
+++ ReactPortfolioProject/src/components/AboutMe.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+// import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap for styling (optional)
 
 export default function AboutMe() {
-    return (
-        <div>
-            <h1>AboutMe Page</h1>
-            <p>This is the About Me/Home page.</p>
-        </div>
-    );
+  return (
+    <div className="container text-center mt-4">
+      <h1>About Me</h1>
+
+      {/* Profile Photo */}
+      <img
+        src="/IMG_2364.jpg"
+        alt="Photo of Liebe"
+        className="img-fluid rounded-circle my-3"
+        style={{
+          width: "200px",
+          height: "200px",
+          objectFit: "cover",
+          border: "3px solid #ddd",
+        }}
+      />
+
+      <p>
+        Hello! I’m a passionate developer excited about building web
+        applications. Welcome to my portfolio!
+      </p>
+    </div>
+  );
 }
